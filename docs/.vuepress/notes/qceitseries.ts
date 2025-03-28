e03f28adb1750428c910380b10d640599940e05b
Calculusai <1697391069@qq.com>
2025-03-28 17:49:35 +0800

    Python2级知识点更新

--- docs/.vuepress/notes/qceitseries.ts
+++ docs/.vuepress/notes/qceitseries.ts
@@ -1,87 +1,100 @@
 import { defineNoteConfig, defineNotesConfig } from 'vuepress-theme-plume'
 
 const qceitseries = defineNoteConfig({
-  dir: 'qceitseries',
-  link: '/qceitseries/',
-  sidebar: [
-    {
-      text:'考级系列',
-      icon:'ri:angularjs-fill',
-      link:"/qceitseries/",
-      items:[
-            //Scratch 开始
+      dir: 'qceitseries',
+      link: '/qceitseries/',
+      sidebar: [
             {
-            text:'Scratch',
-            icon:'simple-icons:scratch',
-            collapsed:true,
-            prefix: '/qceitseries/Scratch',
-            items:[
-            {
-            text:'Scratch一级',
-            icon:'simple-icons:scratch',
-            collapsed:true,
-            prefix: '/qceitseries/Scratch/Scratch1',
-            items: [
-                  'Scratch11', 
-                  'Scratch12'
-                  ],
-            },
-            ]},
-            //Scratch 结束
-            //Python开始
-            {
-            text:'Python',
-            icon:'akar-icons:python-fill',
-            collapsed:true,
-            prefix: '/qceitseries/Python',
-            items:[
-            {
-                text:'Python一级',
-                icon:'akar-icons:python-fill',
-                collapsed:true,
-                prefix: '/qceitseries/Python/python1',
-            items: [
-                  'python11', 
-                  'python12'
-                  ],
-            },
-            {
-                text:'Python三级',
-                icon:'akar-icons:python-fill',
-                collapsed:true,
-                prefix: '/qceitseries/Python/python3',
-            items: [
-                  'python31', 
-                  'python32'
-                  ],
-            },
+                  text: '考级系列',
+                  icon: 'ri:angularjs-fill',
+                  link: "/qceitseries/",
+                  items: [
+                        //Scratch 开始
+                        {
+                              text: 'Scratch',
+                              icon: 'simple-icons:scratch',
+                              collapsed: true,
+                              prefix: '/qceitseries/Scratch',
+                              items: [
+                                    {
+                                          text: 'Scratch一级',
+                                          icon: 'simple-icons:scratch',
+                                          collapsed: true,
+                                          prefix: '/qceitseries/Scratch/Scratch1',
+                                          items: [
+                                                'Scratch11',
+                                                'Scratch12'
+                                          ],
+                                    },
+                              ]
+                        },
+                        //Scratch 结束
+                        //Python开始
+                        {
+                              text: 'Python',
+                              icon: 'akar-icons:python-fill',
+                              collapsed: true,
+                              prefix: '/qceitseries/Python',
+                              items: [
+                                    {
+                                          text: 'Python一级',
+                                          icon: 'akar-icons:python-fill',
+                                          collapsed: true,
+                                          prefix: '/qceitseries/Python/python1',
+                                          items: [
+                                                'python11',
+                                                'python12'
+                                          ],
+                                    },
+                                    {
+                                          text: 'Python二级',
+                                          icon: 'akar-icons:python-fill',
+                                          collapsed: true,
+                                          prefix: '/qceitseries/Python/python2',
+                                          items: [
+                                                'python21',
+                                                'python22'
+                                          ],
+                                    },
+                                    {
+                                          text: 'Python三级',
+                                          icon: 'akar-icons:python-fill',
+                                          collapsed: true,
+                                          prefix: '/qceitseries/Python/python3',
+                                          items: [
+                                                'python31',
+                                                'python32'
+                                          ],
+                                    },
 
-            ]},
-            //Python结束
-            //CPP开始
-            {
-            text:'CPP',
-            icon:'mdi:language-cpp',
-            collapsed:true,
-            prefix: '/qceitseries/CPP',
-            items:[
-            {
-            text:'CPP三级',
-            icon:'mdi:language-cpp',
-            collapsed:true,
-            prefix: '/qceitseries/CPP/CPP3',
-            items: [
-                  'CPP31', 
-                  'CPP32'
+                              ]
+                        },
+                        //Python结束
+                        //CPP开始
+                        {
+                              text: 'CPP',
+                              icon: 'mdi:language-cpp',
+                              collapsed: true,
+                              prefix: '/qceitseries/CPP',
+                              items: [
+                                    {
+                                          text: 'CPP三级',
+                                          icon: 'mdi:language-cpp',
+                                          collapsed: true,
+                                          prefix: '/qceitseries/CPP/CPP3',
+                                          items: [
+                                                'CPP31',
+                                                'CPP32'
+                                          ],
+                                    },
+                              ]
+                        },
+                        //CPP结束
                   ],
             },
-            ]},
-            //CPP结束
-            ],
-    },
-  ]
+      ]
 })
 
 // ... other code
 
-export default  qceitseries;
+export default qceitseries;
---
+++ docs/notes/qceitseries/Python/python2/python21.md
@@ -0,0 +1,6 @@
+---
+title: Python2级算法题
+createTime: 2025/03/21 12:33:09
+icon: tabler:rosette-number-2
+permalink: /qceitseries/Python/python2/python21.html
+---
